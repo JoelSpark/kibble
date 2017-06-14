25beaa9a915004cefab05cccbe52230785af9477
joel <joel@spire.com>
2017-06-14 19:34:13 +0800

    reorganized table

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ import re
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 # application-specific imports
+from flask_login import LoginManager
+
 
 # Set up Flask app
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_message = "You must be logged in to access this page."
+login_manager.login_view = "auth.login"
 
 
 @app.route('/')
